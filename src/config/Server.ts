bb30d6d8ba6d0c4e095af18420b3d1fa71dae66e
alexeyshevin <ashevin92@gmail.com>
2024-06-24 14:41:03 +0700

    Pre-debugging commit

--- src/config/Server.ts
+++ src/config/Server.ts
@@ -1,13 +1,21 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 import { Routes } from "../routes/index";
 import cors from "cors";
 import { Database } from "./Database";
+import * as http from "http";
 
 export default class Server {
+  public port: number = 5005;
+  public httpServer: any;
+
   constructor (app: Application) {
     this.config(app);
     this.synchronizeDatabase();
+    this.httpServer = http.createServer(app);
     new Routes(app);
+    app.get("/", (req: Request, res: Response) => {
+      res.send('Test response.')
+    });
   };
 
   private config (app: Application) : void {
@@ -22,7 +30,7 @@ export default class Server {
     }));
   };
 
-  private async synchronizeDatabase () {
+  private async synchronizeDatabase () : Promise<void> {
     const db = new Database();
     if (db.sequelize !== undefined) {
       await db.sequelize.sync();
--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,7 @@ const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 5005;
 app.listen(PORT, "localhost", () => {
   console.log(`User service is running on port ${PORT}.`);
 });
+
+process.on('uncaughtException', (err) => {
+  console.log(err);
+});
